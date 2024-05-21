2a23813b587059c22321f6cf618b1405ccb8a904
Jonator3 <joey.pehlke@gmail.com>
2024-05-21 03:22:58 +0200

    Update esp_now_bridge.py

--- Python/esp_now_bridge.py
+++ Python/esp_now_bridge.py
@@ -2,11 +2,12 @@ import time
 
 import serial
 from threading import Thread
+import warnings
 
 
 class ESP_NOW_BRIDGE(object):
 
-    def __init__(self, port="/dev/ttyUSB0", baudrate=230400, *, on_receive=lambda data, source: None, on_send=lambda success: None, debug=False):
+    def __init__(self, port="/dev/ttyUSB0", baudrate=230400, *, on_receive=lambda data, source: None, on_send=lambda state, msg, target: None, debug=False):
         self.esp = serial.Serial(port, baudrate)
         self.port = port
         self.baudrate = baudrate
@@ -17,6 +18,7 @@ class ESP_NOW_BRIDGE(object):
 
         self.default_target = "ff:ff:ff:ff:ff:ff"
         self.mac = None
+        self.send_history = []
 
         self.thread = Thread(target=self._loop)
         self.thread.daemon = True
@@ -43,6 +45,7 @@ class ESP_NOW_BRIDGE(object):
 
         byte_data = mac.to_bytes(6, byteorder="big")+ len(data).to_bytes(1)+ data +"\n".encode("ascii")
         self.esp.write(byte_data)
+        self.send_history.append((data, target))
         if self.debug:
             print(">>>", byte_data)
 
@@ -52,19 +55,23 @@ class ESP_NOW_BRIDGE(object):
             if self.debug:
                 print("<<<", data)
             if data.startswith(b'msg:'):
-                self.on_receive(data[10:-2], data[4:10].hex(":", 1))
+                if self.on_receive is not None:
+                    self.on_receive(data[10:-2], data[4:10].hex(":", 1))
             elif data.startswith(b'snd:'):
-                self.on_send(data[4:-2] == b'1')
+                if self.on_send is not None:
+                    self.on_send(data[4:-2] == b'1', *self.send_history[0])
+                del self.send_history[0]
             elif data.startswith(b'mac:'):
-                self.mac = data[4:-2].decode("ascii")
+                self.mac = data[4:-2].decode("ascii").lower()
+            else:  # unexpected Message!
+                warnings.warn("ESP-NOW Bridge received invalid data")
 
 
 def decoder(byte_str):
     msg = ""
     for byte in byte_str:
-        if byte >= 128:
-            break
-        msg += chr(byte)
+        if 10 < byte < 128:
+            msg += chr(byte)
     return msg
 
 
@@ -73,11 +80,12 @@ if __name__ == "__main__":
         print(source, ">>", decoder(msg))
 
 
-    def sender(state):
+    def sender(state, msg, target):
         if not state:
-            print("Sending failed!")
+            print("\033[93mSending failed! ::\t"+target+" >>> "+str(msg)+"\033[0m")
     esp = ESP_NOW_BRIDGE(on_receive=receiver, on_send=sender)
     print("Running ESP-NOW Bridge on:", esp.mac)
     while True:
         msg = input()
-        esp.send(msg)
+        if msg != "":
+            esp.send(msg)
